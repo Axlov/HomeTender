aecf7a03af873dec2f179e70c80ab2b0f1639cc6
Axlov <109531985+Axlov@users.noreply.github.com>
2022-10-29 14:49:37 -0300

    Add files via upload

---
+++ public/app.js
@@ -0,0 +1,113 @@
+"use strict";
+
+// ////////////////////////////////////////////////////////////////////////////////////////////////////////////BACK/////////////////////////////////////////////
+////////////////////////////EXPRESS REQUIRE////////////////////////////////////
+
+// const express = require("express");
+// const https = require("https");
+// const bodyParser = require("body-parser");
+
+// const urlRandom = "https://www.thecocktaildb.com/api/json/v1/1/random.php";
+
+// const app = express();
+
+// app.get("/index.html", function (req, res) {
+//   https.get(urlRandom, function (response) {
+//     response.on("data", function (data) {
+//       const randomDrink = JSON.parse(data);
+//       const drinkName = randomDrink.strDrink;
+//       console.log(drinkName);
+//     });
+//   });
+// });
+
+// app.listen(5500, function () {
+//   console.log("Server is running on port 5500");
+// });
+
+// /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////FRONT///////////////////////////////////////
+
+if (typeof window !== "undefined") {
+  // const nextBtn = document.getElementsByClassName("next")[0];
+  const nextBtn = document.querySelector(".next");
+  const prevBtn = document.querySelector(".prev");
+  const slides = document.querySelectorAll(".carousel-image");
+  const container = document.querySelector(".carousel");
+  const dots = [...document.querySelectorAll(".dot")];
+
+  const dotOne = dots[0];
+  const dotTwo = dots[1];
+  const dotThree = dots[2];
+
+  console.log(nextBtn, prevBtn, slides, container, dots);
+  // let curSlide = 0;
+
+  ///////////////////// Carousel Function ////////////////////////////////
+
+  // slides vars
+  let curSlide = 2;
+  const maxSlide = slides.length - 1;
+
+  ///////Function Active Dots///////////////////
+
+  function activeDot(curSlide) {
+    dots.forEach((dot, index) => {
+      if (index === curSlide) {
+        dot.classList.toggle("active-dot");
+      } else {
+        dot.classList.remove("active-dot");
+      }
+    });
+  }
+
+  // //////Function that moves carousel/////////
+
+  function moveCarousel(curSlide) {
+    slides.forEach((slide, index) => {
+      // loop on the array of slides and moovef
+
+      slide.style.transform = `translateX(${(index - curSlide) * 100}%)`;
+
+      //(index-curSlide) to always show slide at translateX(0%)
+    });
+    activeDot(curSlide);
+  }
+
+  // /////////////////AutoMove Carousel///////////////////////////////
+
+  // moves carousel auto every 3 s
+
+  setInterval(() => {
+    curSlide === maxSlide ? (curSlide = 0) : curSlide++;
+    moveCarousel(curSlide);
+  }, 3000);
+
+  // Buttons Functionality
+  nextBtn.addEventListener("click", function () {
+    // if the current slide is the maxSlide return to slide0 else current slide+1
+    curSlide === maxSlide ? (curSlide = 0) : curSlide++;
+    moveCarousel(curSlide);
+  });
+
+  prevBtn.addEventListener("click", function () {
+    // same as with maxSlide
+    curSlide === 0 ? (curSlide = maxSlide) : curSlide--;
+    moveCarousel(curSlide);
+  });
+
+  dotOne.addEventListener("click", function () {
+    //
+    curSlide = 0;
+    moveCarousel(curSlide);
+  });
+
+  dotTwo.addEventListener("click", function () {
+    curSlide = 1;
+    moveCarousel(curSlide);
+  });
+
+  dotThree.addEventListener("click", function () {
+    curSlide = 2;
+    moveCarousel(curSlide);
+  });
+}
